29d52e52d1e4fc9993dc48faf6d4347461bd32db
jspe3ab15 <jspe3ab15@scs.local>
2024-06-20 11:44:42 +0800

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,26 @@
+@extends('mainLayout')
+
+@section('page-title','Account Login')
+
+@section('auth-content')
+
+    <form method="POST" action="{{ route('login') }}">
+    @csrf
+    <div>
+        <label class="auth-labels">Username</label>
+        <input type="text" name="name" value="{{ old('username') }}" required class="authtextbox">
+
+    @error('name')
+        <span>{{ $message }}</span>
+    @enderror
+    </div>
+    <div>
+        <label class="auth-labels">Password</label>
+        <input type="password" name="password" required class="auth-textbox">
+        @error('password')
+            <span>{{ $message }}</span>
+        @enderror
+    </div>
+    <button type="submit">Login</button>
+</form>
+@endsection 
